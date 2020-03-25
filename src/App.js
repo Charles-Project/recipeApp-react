931a00d198cd5d007b6f8d660faf4bb4c3d71d9f
ErnestMarc <ernestpberry@gmail.com>
2020-03-25 15:56:23 +0100

    Finish the api calls to display on the ui

--- src/App.js
+++ src/App.js
@@ -1,68 +1,61 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./component/Navbar";
-import Data from "./component/data";
+// import Data from "./component/data";
 
 const App = () => {
   const APP_ID = "41e0b3e2";
   const APP_KEY = "64a94f6011150d648f1276269554fd89";
-  const proxy = "https://cors-anywhere.herokuapp.com/";
+  // const proxy = "https://cors-anywhere.herokuapp.com/";
   let url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-  // https://api.edamam.com/search?q=chicken&app_id=${41e0b3e2}&app_key=${709f13646f0bdef4cdb462f1a3ef55d5}
-
-  // const appkey = "ad45fbf063cb4caf87f795843e7288af";
-  // let url = `https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=${appkey}`;
-
-  const [recipes, setRecipe] = useState([Data]);
+  const [recipes, setRecipe] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
   useEffect(() => {
+    url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
     getRecipe();
-  }, []);
+  }, [query]);
 
   const getRecipe = async () => {
-    console.log("total items", recipes);
-    console.log("total itemsz", recipes[0]);
+    const response = await fetch(url);
+    const data = await response.json();
+    setRecipe(data.hits);
+  };
 
-    recipes[0].map((value, i) => {
-      console.log(value.label);
-      i++;
-    });
+  const changeHandler = e => {
+    setSearch(e.target.value);
+  };
 
-    // const response = await fetch(url);
-    // const data = await response.json();
-    // console.log(data.hits);
-    // setRecipe(data.hits);
-    // fetch(url)
-    //   .then(response => {
-    //     return response.json();
-    //   })
-    //   .then(data => {
-    //     setRecipe(data.hits);
-    //     console.log(data.hits);
-    //   })
-    //   .catch(err => {
-    //     console.log(err);
-    //   });
+  const changeQuery = () => {
+    setQuery(search);
   };
 
   return (
     <div className="App">
       <div className="container-fluid px-0 w-100 overflow-hidden-x">
-        <Navbar />
+        <Navbar
+          search={search}
+          changeHandler={changeHandler}
+          changeQuery={changeQuery}
+        />
         <div className="row mt-3">
-          {recipes[0].map(item => (
-            <div className="col-sm-6 col-lg-4 col-xl-3 mt-3 mb-3 d-flex justify-content-center">
+          {recipes.map(item => (
+            <div
+              className="col-sm-6 col-lg-4 col-xl-3 mt-3 mb-3 d-flex justify-content-center"
+              key={item.recipe.label}
+            >
               <div className="card-wrapper ">
                 <div className="img-wrapper">
-                  <img src={item.image} alt="" />
+                  <img src={item.recipe.image} alt="" />
                 </div>
                 <div className="card-body px-3">
                   <h5 className="card-title mb-4 text-pacifico">
-                    {item.label}
+                    {item.recipe.label}
                   </h5>
                   <button className="btn-view">View More</button>
-                  <a href={item.url} className="float-right btn-url">
+                  <a href={item.recipe.url} className="float-right btn-url">
                     Goto Url
                   </a>
                 </div>
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,14 +1,22 @@
 import React from "react";
 
-function Navbar() {
+function Navbar({ search, changeHandler, changeQuery }) {
   return (
     <div className="py-3 bg-light">
       <h1 className="brand-logo text-righteous text-tomatoes mb-4">
         Recipe App
       </h1>
       <div className="search-wrapper mb-2 d-flex justify-content-center align-items-center">
-        <input type="text" placeholder="search..." />
-        <button className="d-flex justify-content-center align-items-center">
+        <input
+          type="text"
+          placeholder="search..."
+          value={search}
+          onChange={changeHandler}
+        />
+        <button
+          className="d-flex justify-content-center align-items-center"
+          onClick={changeQuery}
+        >
           <i className="fas fa-search"></i>
         </button>
       </div>
